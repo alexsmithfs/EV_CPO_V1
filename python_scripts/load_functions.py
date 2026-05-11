ddaba7fa10c0e0ec850b4add68cc7265d0628ffd
Alex Smith <alexsmithfs@gmail.com>
2026-05-11 20:35:42 +0100

    Added load functions for appended data tables

--- python_scripts/load_functions.py
+++ python_scripts/load_functions.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 db_config = {
     "user": "postgres",
@@ -11,14 +11,28 @@ db_config = {
 
 engine = create_engine(f"postgresql://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}")
 
+# Functions 
+# _____________________________________________________________________________________________
+
+# Function to load data into SQL database
 def load_db(df, table_name):
     df.to_sql(table_name, engine, if_exists='replace', index=False)
 
 
-def load_db_append(table_name, table_name_append):
+# Function to append only new records from the main table to the append table
+def load_db_append(table_name):
 
-    # Using SQL query to dynimcally append only new records from the main table to the append table
-    sql_query = f'''
+    table_name_append = f"{table_name}_append"
+
+    # This SQL command does the work internally in Postgres
+    sql_command = text(f"""
+        INSERT INTO {table_name_append} (
+            transaction_id, 
+            date_timestamp, 
+            charger_id, 
+            cost, 
+            dwh_date_added
+        )
         SELECT 
             t.transaction_id, 
             t.date_timestamp, 
@@ -28,16 +42,25 @@ def load_db_append(table_name, table_name_append):
         FROM {table_name} t
         LEFT JOIN {table_name_append} t_append
             ON (t.transaction_id = t_append.transaction_id) AND (t.date_timestamp = t_append.date_timestamp) AND (t.charger_id = t_append.charger_id) AND (t.cost = t_append.cost)
-        WHERE t_append.transaction_id IS NULL;
-    '''
-    df = pd.read_sql(sql_query, engine)
-    df.to_sql(table_name_append, engine, if_exists='append', index=False)
+        WHERE t_append.transaction_id IS NULL
+        ;
+    """)
+
+    # Use engine.begin() to ensure the transaction is committed
+    with engine.begin() as connection:
+        result = connection.execute(sql_command)
+        print(f"Transfer complete. Appended {result.rowcount} new records from {table_name} to {table_name_append}.")
 
 
-file_path = R'C:\Users\alexs\OneDrive\Documents\Data Engineering\EV_CPO_V1\Raw Data Files\Revenue_source_3.csv'
-raw_data = pd.read_csv(file_path)
+# Testing 
+# _____________________________________________________________________________________________
 
-print(raw_data.info())
+if __name__ == "__main__":
 
-load_db(raw_data, 'rev_source_3')
-load_db_append('rev_source_3', 'rev_source_3_append')    
+    file_path = R'C:\Users\alexs\OneDrive\Documents\Data Engineering\EV_CPO_V1\Raw Data Files\Revenue_source_3.csv'
+    raw_data = pd.read_csv(file_path)
+
+    print(raw_data.info())
+
+    load_db(raw_data, 'rev_source_3')
+    load_db_append('rev_source_3') 
--- python_scripts/raw_data_etl_pipeline.py
+++ python_scripts/raw_data_etl_pipeline.py
@@ -1,14 +1,22 @@
 from extract_functions import extract_csv
 from load_functions import load_db, load_db_append 
 
+# Extract
+# _____________________________________________________________________________________________
 raw_rev_1_data = extract_csv(R'C:\Users\alexs\OneDrive\Documents\Data Engineering\EV_CPO_V1\Raw Data Files\Revenue_source_1.csv')
 raw_rev_2_data = extract_csv(R'C:\Users\alexs\OneDrive\Documents\Data Engineering\EV_CPO_V1\Raw Data Files\Revenue_source_2.csv')
 raw_rev_3_data = extract_csv(R'C:\Users\alexs\OneDrive\Documents\Data Engineering\EV_CPO_V1\Raw Data Files\Revenue_source_3.csv')
 
+# No need to transorm as the purpose of this data is to be the raw and untouched data
+# _____________________________________________________________________________________________
+
+# Load
+# _____________________________________________________________________________________________
 load_db(raw_rev_1_data, 'rev_source_1')
 load_db(raw_rev_2_data, 'rev_source_2')
 load_db(raw_rev_3_data, 'rev_source_3')
 
-load_db_append('rev_source_1', 'rev_source_1_append')
-load_db_append('rev_source_2', 'rev_source_2_append')
-load_db_append('rev_source_3', 'rev_source_3_append')
+load_db_append('rev_source_1')
+load_db_append('rev_source_2')
+load_db_append('rev_source_3')
+
