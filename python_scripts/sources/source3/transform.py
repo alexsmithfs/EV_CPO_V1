0b4556d0a11f7880e0266269dab9041602c2e156
Alex Smith <alexsmithfs@gmail.com>
2026-08-18 13:55:29 +0100

    Deleted unecasary files

--- python_scripts/sources/source3/transform.py
+++ python_scripts/sources/source3/transform.py
@@ -13,6 +13,16 @@ def transform_rev_src_3(df):
     # Filtering only to transactions that have succeeded or are Pending. We do not want to include failed transactions in our analysis
     df = df[df['transaction_tag'].isin(['Succeeded', 'Pending', 'Refunded'])]
 
+    # Converting column types
+    df = df.astype({
+        'transaction_id': 'str',
+        'charger_id': 'str',
+        'cost': 'float64'  # In pandas/NumPy, 'float64' is the equivalent of a SQL Double
+    })
+
+    # Convert the date column to a proper timestamp
+    df['date_timestamp'] = pd.to_datetime(df['date_timestamp'])
+
     # Update the 'cost' column for 'Pending' transactions as we haven't actually recieved payment yet.
     df.loc[df['transaction_tag'] == 'Pending', 'cost'] = 0
 
